7c9fed0a361ee10eb4af5ae2f8c15c0b51511125
rupesh <rpiwal02@gmail.com>
2024-12-25 23:33:45 +0530

    protectRoute middleware

--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
+import { clerkMiddleware } from "@clerk/express";
+
 import userRoutes from "../routes/user.route.js";
 import adminRoutes from "../routes/admin.route.js";
 import albumRouts from "../routes/album.route.js";
@@ -11,9 +13,10 @@ import { connectDB } from "../lib/db.js";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 8001;
+const PORT = process.env.PORT || 4001;
 
 app.use(express.json());
+app.use(clerkMiddleware()); // this will add auth to req obj => req.auth.userId
 
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
@@ -24,5 +27,5 @@ app.use("/api/stats", statRoutes);
 
 app.listen(PORT, () => {
   console.log(`app is listening on port: ${PORT}`);
-  connectDB()
+  connectDB();
 });

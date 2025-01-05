6f7a3fc3457a524d6e2aa5fe0673de9187d7bcb5
rupesh <rpiwal02@gmail.com>
2025-01-05 15:57:30 +0530

    minor config

--- server/src/routes/user.route.js
+++ server/src/routes/user.route.js
@@ -1,5 +1,7 @@
 import { Router } from "express";
+import { getAllUsers } from "../controllers/user.controller";
+import { protectRoute } from "../middleware/auth.middleware";
 
 const router = Router();
-router.get("/", (req, res) => {})
+router.get("/", protectRoute, getAllUsers);
 export default router;

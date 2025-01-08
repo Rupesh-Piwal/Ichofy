abfd600bf1c6395d8231e09a8581ae3b13dc54ff
rupesh <rpiwal02@gmail.com>
2025-01-08 15:12:54 +0530

    changes in playlist skeleton style

--- client/src/components/skeletons/PlaylistSkeleton .tsx
+++ client/src/components/skeletons/PlaylistSkeleton .tsx
@@ -1,10 +1,10 @@
 const PlaylistSkeleton = () => {
   return Array.from({ length: 7 }).map((_, i) => (
     <div key={i} className="p-2 rounded-md flex items-center gap-3">
-      <div className="w-12 h-12 bg-gradient-to-r from-[#B5179E]/40 to-[#7209B7]/30 rounded-md flex-shrink-0 animate-pulse" />
+      <div className="w-12 h-12 bg-gradient-to-r from-[#B5179E]/40 bg-slate-600 rounded-md flex-shrink-0 animate-pulse" />
       <div className="flex-1 min-w-0 hidden md:block space-y-2">
-        <div className="h-4 bg-gradient-to-r from-[#B5179E]/40 to-[#7209B7]/30 rounded animate-pulse w-3/4" />
-        <div className="h-3 bg-gradient-to-r from-[#B5179E]/40 to-[#7209B7]/30 rounded animate-pulse w-1/2" />
+        <div className="h-4 bg-gradient-to-r from-[#B5179E]/40 bg-slate-600 rounded animate-pulse w-3/4" />
+        <div className="h-3 bg-gradient-to-r from-[#B5179E]/40 bg-slate-600 rounded animate-pulse w-1/2" />
       </div>
     </div>
   ));

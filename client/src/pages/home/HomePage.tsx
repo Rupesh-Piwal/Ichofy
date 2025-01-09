ef58544696fb6f8fb27ba6a18e41bab1bdede0fb
rupesh <rpiwal02@gmail.com>
2025-01-09 15:51:31 +0530

    featured section topography updated

--- client/src/pages/home/HomePage.tsx
+++ client/src/pages/home/HomePage.tsx
@@ -24,13 +24,14 @@ const HomePage = () => {
   console.log({ fetchFeaturedSongs, fetchMadeForYouSongs, fetchTrendingSongs });
 
   return (
-    <main className="rounded-md overflow-hidden h-full bg-gradient-to-b from-zinc-800 to-zinc-900">
+    <main className="rounded-md overflow-hidden h-full bg-gradient-to-r from-[#7209B7]/10 to-[#B5179E]/10">
       <Topbar />
       <ScrollArea className="h-[calc(100vh-180px)]">
         <div className="p-4 sm:p-6">
-          <h1 className="text-2xl sm:text-3xl font-bold mb-6">
-            Good afternoon
+          <h1 className="text-2xl text-center sm:text-3xl md:text-4xl font-bold mb-6 bg-gradient-to-r from-purple-900 to-pink-700 bg-clip-text text-transparent font-['Great_Vibes'] tracking-wide">
+            Vibe with us...
           </h1>
+
           <FeaturedSection />
 
           <div className="space-y-8">
@@ -39,7 +40,7 @@ const HomePage = () => {
               songs={madeForYouSongs}
               isLoading={isLoading}
             />
-            < SectionGrid
+            <SectionGrid
               title="Trending"
               songs={trendingSongs}
               isLoading={isLoading}

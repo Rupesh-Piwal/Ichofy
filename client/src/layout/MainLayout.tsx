333ac0615c5dd048900795af493117a2b8b33586
rupesh <rpiwal02@gmail.com>
2025-01-12 19:32:20 +0530

    feat: implemented play-back controller feature

--- client/src/layout/MainLayout.tsx
+++ client/src/layout/MainLayout.tsx
@@ -7,8 +7,8 @@ import { Outlet } from "react-router-dom";
 import LeftSidebar from "./components/LeftSidebar";
 import FriendsActivity from "./components/FriendsActivity ";
 import AudioPlayer from "./components/AudioPlayer";
-// import { PlaybackControls } from "./components/PlaybackControls";
 import { useEffect, useState } from "react";
+import { PlaybackControls } from "./components/PlaybackControls";
 
 const MainLayout = () => {
   const [isMobile, setIsMobile] = useState(false);
@@ -63,7 +63,7 @@ const MainLayout = () => {
         )}
       </ResizablePanelGroup>
 
-      {/* <PlaybackControls /> */}
+      <PlaybackControls />
     </div>
   );
 };
